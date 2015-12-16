b49aecfe68d491562e50313bbe6ee984327caf6e
Phvnux <dvnean@gmail.com>
2015-12-16 11:22:59 +0800

    head first pytho -- nester version-1.0.0

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester',
+	version		= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'phvn',
+	author_email= 'xxxxxxxxx@xxx.com',
+	url			= 'http://xxxxxxxx.com',
+	description = 'test',
+)
